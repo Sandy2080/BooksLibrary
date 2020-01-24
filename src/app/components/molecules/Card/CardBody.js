d31516fa6cd903359fa80c2ac4513a75af804f11
Sandy Ludosky <sandyludosky@gmail.com>
2020-01-24 13:43:03 +0100

    home : responsiveness to fit on smaller screen

--- src/app/components/molecules/Card/CardBody.js
+++ src/app/components/molecules/Card/CardBody.js
@@ -3,17 +3,24 @@ import { useDispatch } from "react-redux";
 import { TextTruncate } from "../../atoms/text/Text";
 import { addToCart } from "../../../lib/actions/shoppingCart";
 import Button, { ButtonSize, ButtonTheme }  from '../../atoms/button/index'
+import useWindowDimensions from '../../../utils/hooks/useWindowDimensions';
 
 const AddToCart = ({ item, children }) => {
   const dispatch = useDispatch()
   const addCart = () => dispatch(addToCart(item))
+  const { width } = useWindowDimensions();
+  const isMobile =  width < 640
+  const { SMALL, LARGE } = ButtonSize
+  const { ROUNDED } = ButtonTheme
   return (
-    <Button.INFO 
-      classNames="float-right" 
+    <Button.INFO  
+      theme={ROUNDED}
+      size={isMobile ? LARGE : SMALL}
+      action={addCart}
       customColor="#2980b9" 
-      theme={ButtonTheme.ROUNDED}
-      size={ButtonSize.SMALL}
-      action={addCart}>
+      classNames={`float-right ${isMobile && `btn-block`}`}
+      >
+
       {children}
     </Button.INFO >
   );
--- src/app/components/molecules/Card/styles.js
+++ src/app/components/molecules/Card/styles.js
@@ -2,4 +2,18 @@ import styled from "styled-components";
 
 export const StyledCard = styled.div` 
     margin-bottom: 20px;
+    .row {
+        margin-bottom: 0!important;
+    }
+    @media screen and (max-width: 640px) {
+        div:first-child {
+            margin-bottom: 40px;
+        }
+        div {
+            margin: 0 auto;
+        }
+        h5, h6 {
+            text-align: center;
+        }
+    }
 `
