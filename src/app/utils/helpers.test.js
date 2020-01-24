e2a5fec3cbb6f97941948fb939a1f691f2c2bae0
Sandy Ludosky <sandyludosky@gmail.com>
2020-01-24 21:21:39 +0100

    added unit tests

---
+++ src/app/utils/helpers.test.js
@@ -0,0 +1,34 @@
+import { calculateTotal,  checkout, calculateBestOffer } from "./helpers"
+import { Offer } from "../models/Offer"
+import * as MOCKS from './mocks'
+
+const { data } = MOCKS
+const { books, offers } = data
+const quantity = 1
+const cart_total = books.map(book => book.price * quantity).reduce(calculateTotal, 0)
+const commercialOffers = offers.map(obj => new Offer(obj))
+const bestOffer = calculateBestOffer(cart_total, commercialOffers)
+
+ describe('calculateTotal', () => {
+     describe('when upon checkout', () => {
+         it('calculate cart total should return 65', () => {
+             expect(cart_total).toEqual(65);
+         });
+     });
+ });
+describe('calculateBestOffer', () => {
+    describe('when no URL', () => {
+        it('calculate best offer should return 15', () => {
+            expect(bestOffer.discountValue).toEqual(15);
+        });
+    });
+});
+
+describe('calculate total with best offer', () => {
+    describe('when no URL', () => {
+        it('total cart after best offer discount should return 50', () => {
+            const cart_total_discount = cart_total - bestOffer.discountValue
+            expect(cart_total_discount).toEqual(50);
+        });
+    });
+});
