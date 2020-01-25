cb9205be52b8611e8a6b94660d098d337aea3311
Sandy Ludosky <sandyludosky@gmail.com>
2020-01-26 00:24:45 +0100

    snapshots tests

---
+++ src/app/components/atoms/button/button.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button, { ButtonTheme,  ButtonSize} from './index';
+import renderer from 'react-test-renderer';
+
+describe('Button Primary', () => {
+  it("renders correctly when button is PRIMARY  ", () => {
+    const tree = renderer.create(<Button.PRIMARY />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Button Rounded", () => {
+  it("renders correctly when button is ROUNDED and LARGE", () => {
+    const tree = renderer
+      .create(
+        <Button.PRIMARY size={ButtonSize.LARGE} theme={ButtonTheme.ROUNDED} />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
