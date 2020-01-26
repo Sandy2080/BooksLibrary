5923b1962a4ee086504bf060bdcfb53bb368dc4f
Sandy Ludosky <sandyludosky@gmail.com>
2020-01-26 19:08:43 +0100

    update README file and add to cart button hover effect

--- src/app/components/organismes/ProductCard.js
+++ src/app/components/organismes/ProductCard.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useDispatch } from "react-redux";
 import { ProductImage } from '../atoms/image/Image'
 import Button, { ButtonSize, ButtonTheme } from '../atoms/button/index'
@@ -8,7 +8,7 @@ import Card, { CardHeader } from '../molecules/Card/index'
 import { addToCart } from "../../lib/actions/shoppingCart";
 import { StyledProductCard } from "./styles"
 
-const AddToCartButton = ({ item, children }) => {
+const AddToCartButton = ({ item, children, isVisible }) => {
   const dispatch = useDispatch()
   const addCart = () => dispatch(addToCart(item))
   const { SMALL, LARGE } = ButtonSize
@@ -20,7 +20,7 @@ const AddToCartButton = ({ item, children }) => {
         size={isMobile ? LARGE : SMALL}
         action={addCart}
         customColor="#2980b9"
-        classNames={`float-right add-cart-button ${isMobile && `btn-block`}`}>
+        classNames={`float-right add-cart-button ${isMobile && `btn-block`} ${isVisible && 'visible'}`}>
         {children}
       </Button.INFO >
     </div>
@@ -37,14 +37,17 @@ const ProductInformation = ({ item, fontSize }) => {
             <Text.TRUNCATED>{synopsis}</Text.TRUNCATED>
           </Fragment>)
 }
-const ProductCard = ({ item }) => (
-    <Card>
-      <ProductImage {...item} grid="col-sm-2"/>
-      <StyledProductCard className="col-sm">
-        <ProductInformation item={item} />
-        <AddToCartButton item={item}>Add to Cart</AddToCartButton>
-      </StyledProductCard>
-    </Card>
-);
+const ProductCard = ({ item }) => {
+  const [isVisible, setVisible] = useState(false)
+  const handleOnHover = (bool) => setVisible(bool)
+  return (<Card>
+    <ProductImage {...item} grid="col-sm-2" />
+    <StyledProductCard className="col-sm " onMouseOver={() => handleOnHover(true)} onMouseLeave={() => handleOnHover(false)}>
+      <ProductInformation item={item} />
+      <AddToCartButton item={item} isVisible={isVisible}>Add to Cart</AddToCartButton>
+    </StyledProductCard>
+  </Card>)
+}
+
 export default ProductCard;
 
--- src/app/components/organismes/styles.js
+++ src/app/components/organismes/styles.js
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 
 export const StyledProductCard = styled.div`
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-     .add-cart-button {
-        float: right;
+    .add-cart-button {
+        position: absolute;
+        opacity: 0;
+        bottom: -20px;
+        right: 20px;
+        transition: all 0.5s;
+    }
+    .add-cart-button.visible {
+        opacity: 1;
+        bottom: 0px;
     }
     @media screen and (max-width: 640px) {
         div:first-child {
