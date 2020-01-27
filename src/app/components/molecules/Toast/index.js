d90fcb61b49dc78ca520ec88bfb23588da8ffb6e
Sandy Ludosky <sandyludosky@gmail.com>
2020-01-27 22:04:38 +0100

    fix warnings

--- src/app/components/molecules/Toast/index.js
+++ src/app/components/molecules/Toast/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect }  from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { StyledToast } from './styles';
 import { Badge, Text } from '../../atoms'
